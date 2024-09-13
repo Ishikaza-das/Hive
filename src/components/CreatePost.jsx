b19139b40e3825dc259132c57476bde86d8c48c2
Ritesh <riteshdas8652@gmail.com>
2024-09-13 10:40:38 +0530

    Naviagte fix

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,89 +1,90 @@
-import { useContext, useRef } from "react";
-import {PostList} from "../store/post-list-store";
-import { useNavigate } from "react-router-dom";
+// import { useContext, useRef } from "react";
+// import {PostList} from "../store/post-list-store";
+import { Form, redirect } from "react-router-dom";
 
 const CreatePost = () => {
-  const {addPost} = useContext(PostList);
-  const navigate = useNavigate();
+  // const {addPost} = useContext(PostList);
+  // const navigate = useNavigate();
 
-  const userIdElement = useRef();
-  const postTitleElement = useRef();
-  const postBodyElement = useRef();
-  const reactionsElement = useRef();
-  const tagsElement = useRef();
+  // const userIdElement = useRef();
+  // const postTitleElement = useRef();
+  // const postBodyElement = useRef();
+  // const reactionsElement = useRef();
+  // const tagsElement = useRef();
 
-  const handelSubmit = (event) => {
-    event.preventDefault();
+  // const handelSubmit = (event) => {
+    // event.preventDefault();
 
-    const userId = userIdElement.current.value;
-    const postTitle = postTitleElement.current.value;
-    const postBody = postBodyElement.current.value;
-    const reactions = reactionsElement.current.value;
-    const tags = tagsElement.current.value.split(" ");
+    // const userId = userIdElement.current.value;
+    // const postTitle = postTitleElement.current.value;
+    // const postBody = postBodyElement.current.value;
+    // const reactions = reactionsElement.current.value;
+    // const tags = tagsElement.current.value.split(" ");
 
-    userIdElement.current.value = "";
-    postTitleElement.current.value = "";
-    postBodyElement.current.value = "";
-    reactionsElement.current.value = "";
-    tagsElement.current.value = "";
+    // userIdElement.current.value = "";
+    // postTitleElement.current.value = "";
+    // postBodyElement.current.value = "";
+    // reactionsElement.current.value = "";
+    // tagsElement.current.value = "";
 
     // console.log("Sending post to server");
     
-    fetch('https://dummyjson.com/posts/add', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        title: postTitle,
-        body: postBody,
-        reactions: reactions,
-        userId: userId,
-        tags: tags,
-      }),
-    })
-    .then(res => res.json())
-    .then(post => {
-      console.log('Got response', post);
-      addPost(post)
-      navigate("/");
-    }  
-    );
-    
-  }
+  // }
 
 
 
   return (
     <>
-      <form className="create-post" onSubmit={handelSubmit}>
+      <Form className="create-post" method="POST">
 
          <div className="mb-3">
           <label htmlFor="userId" className="form-label">User Id</label>
-          <input type="text" className="form-control" id="userId" aria-describedby="emailHelp" placeholder="Your user Id" ref={userIdElement}/>
+          <input type="text" className="form-control" id="userId" aria-describedby="emailHelp" placeholder="Your user Id" name="userId"/>
         </div>
 
         <div className="mb-3">
           <label htmlFor="title" className="form-label">Post Title</label>
-          <input type="text" className="form-control" id="title" aria-describedby="emailHelp" placeholder="Share Your Thoughts..." ref={postTitleElement}/>
+          <input type="text" className="form-control" id="title" aria-describedby="emailHelp" placeholder="Share Your Thoughts..." name="title"/>
         </div>
         
          <div className="mb-3">
           <label htmlFor="body" className="form-label">Post Content</label>
-          <textarea type="text" rows="4"className="form-control" id="body" aria-describedby="emailHelp" placeholder="Tell us more of it..." ref={postBodyElement}/>
+          <textarea type="text" rows="4"className="form-control" id="body" aria-describedby="emailHelp" placeholder="Tell us more of it..." name="body"/>
         </div>
 
          <div className="mb-3">
           <label htmlFor="reactions" className="form-label">Number of reactions</label>
-          <input type="text" className="form-control" id="reactions" aria-describedby="emailHelp" placeholder="How many people reacted" ref={reactionsElement}/>
+          <input type="text" className="form-control" id="reactions" aria-describedby="emailHelp" placeholder="How many people reacted" name="reactions"/>
         </div>
 
          <div className="mb-3">
           <label htmlFor="tags" className="form-label">Hash Tags</label>
-          <input type="text" className="form-control" id="tags" aria-describedby="emailHelp" placeholder="Please enter tags using space..." ref={tagsElement}/>
+          <input type="text" className="form-control" id="tags" aria-describedby="emailHelp" placeholder="Please enter tags using space..." name="tags"/>
         </div>
         <button type="submit" className="btn btn-primary">Post</button>
-    </form>
+    </Form>
     </>
   );
 };
+
+export async function createPostAction(data) {
+  const formData = await data.request.formData();
+  const postData = Object.fromEntries(formData);
+  postData.tags = postData.tags.split(" ");
+  console.log(postData);
+  
+ fetch('https://dummyjson.com/posts/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(postData),
+    })
+    .then(res => res.json())
+    .then(post => {
+      console.log(post);
+    }  
+    );
+
+  return redirect("/");
+}
 
 export default CreatePost;
